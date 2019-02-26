fba037b3d493a08a1a316fa5b3cc2a5d821386df
Angel <1049547382@qq.com>
2019-02-26 22:20:59 +0800

    update *

--- I-mooc/rem/js/alert.js
+++
@@ -1,7 +0,0 @@
-!function(){
-	var pic = $('#pic');
-	var alertImg = $('<a href="http://zidingyi.mikecrm.com/TieJjIb"><img class="alert" src= "http://p7kyjkmgh.bkt.clouddn.com/alertre.png?time='+new Date().getTime()+'"><'+'/'+'></a>');
-	pic.append(alertImg)
-
-}()
-
