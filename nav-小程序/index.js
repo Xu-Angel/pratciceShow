e1efc28b68f70f780bd42a99491cf4dba6c53924
Angel <1049547382@qq.com>
2019-10-13 17:42:23 +0800

    update

--- nav-小程序/index.js
+++ nav-小程序/index.js
@@ -1,7 +1,7 @@
 const app = new getApp()
 import { UserModel } from '../../models/user.js'
 const user = new UserModel()
-let dialog, toast
+// let DIALOG
 
 Component({
   /**
@@ -13,8 +13,7 @@ Component({
       color: '#999',
       selectedColor: '#1B82D2',
       currentPagePath: '/pages/home/home',
-      list: [
-        {
+      list: [{
           id: 'home',
           pagePath: '/pages/home/home',
           iconPath: 'images/home.png',
@@ -44,7 +43,8 @@ Component({
           iconPath: 'images/found.png',
           handler: 'handleFound',
           selectedIconPath: 'images/found-active.png',
-          text: '发现'
+          text: '发现',
+          isHome: true
         },
         {
           id: 'my',
@@ -69,18 +69,83 @@ Component({
    */
 
   ready() {
-    toast = this.selectComponent('#nav-toast')
+    // DIALOG = this.selectComponent('#Nav-Dialog')
   },
   methods: {
+    // DialogEventConfirm() {
+    //   DIALOG.success()
+    // },
+    // DialogEventCancel() {
+    //   DIALOG.fail()
+    // },
+    /**
+     * 返回用户状态
+     */
+    userPerfectInfo(data = {}) {
+      return new Promise((resolve, reject) => {
+        // 请求响应
+        wx.showLoading({
+          title: '鉴权中',
+          mask: true
+        })
+        user.getPerfectInfo(data).then(function (res) {
+          wx.hideLoading()
+          if (res.result) {
+            resolve(res.result)
+          }
+          if (res.error) {
+            wx.showToast({
+              title: res.error.message,
+              icon: 'none',
+              duration: 2000
+            })
+          }
+        })
+      })
+    },
     handleHome() {
       wx.switchTab({
         url: '/pages/home/home'
       })
     },
-    handleBusiness() {
-      wx.switchTab({
-        url: '/pages/business/business'
-      })
+    handleBusiness(e) {
+      const userInfo = wx.getStorageSync('userInfo')
+      if (e.detail.userInfo) {
+        if (userInfo && userInfo.id) {
+          //验证用户资料状态
+          this.userPerfectInfo({ id: userInfo.id }).then(rs => {
+            const { isCert, isPerfect, isVip } = rs
+            console.log(isCert, '这是组件方法 通用权限鉴定')
+            if (!isCert) {
+              wx.showModal({
+                title: '',
+                content: '您没有权限，请先进行企业认证',
+                cancelText: '取消',
+                cancelColor: '#000000',
+                confirmText: '立即认证',
+                confirmColor: '#007dfc',
+                success (res) {
+                  if (res.confirm) {
+                    wx.navigateTo({
+                      url: "/pages/my-auth/my-auth?edit=now"
+                    })
+                  } else if (res.cancel) {
+                    console.log('用户点击取消')
+                  }
+                }
+              })
+            } else {
+              wx.switchTab({
+                url: "/pages/business/business"
+              })
+            }
+          })
+        } else {
+          wx.navigateTo({
+            url: '/pages/login/login'
+          })
+        }
+      }
     },
     handleFound() {
       wx.switchTab({
@@ -89,11 +154,36 @@ Component({
     },
     handleRelease(e) {
       const userInfo = wx.getStorageSync('userInfo')
-      // const did = this.data.tabbar.list.filter(item => item.pagePath === this.data.currentPagePath)[0].id
-      // dialog = this.selectComponent(`#nav-dialog-${did}`);
       if (e.detail.userInfo) {
         if (userInfo && userInfo.id) {
-          this.triggerEvent('release', { userInfo: userInfo })
+          //验证用户资料状态
+          this.userPerfectInfo({ id: userInfo.id }).then(rs => {
+            const { isCert, isPerfect, isVip } = rs
+            console.log(isCert, '这是组件方法 通用权限鉴定--')
+            if (!isCert) {
+              wx.showModal({
+                title: '',
+                content: '您尚未完成企业认证，暂无法发布需求',
+                cancelText: '取消',
+                cancelColor: '#000000',
+                confirmText: '立即认证',
+                confirmColor: '#007dfc',
+                success (res) {
+                  if (res.confirm) {
+                    wx.navigateTo({
+                      url: "/pages/my-auth/my-auth?edit=now"
+                    })
+                  } else if (res.cancel) {
+                    console.log('用户点击取消')
+                  }
+                }
+              })
+            } else {
+              wx.navigateTo({
+                url: "/pages/issue-demand/issue-demand"
+              })
+            }
+          })
         } else {
           wx.navigateTo({
             url: '/pages/login/login'
